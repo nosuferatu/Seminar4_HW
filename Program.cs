4489b933448e0840a74584eecbc6efd6c888a297
nosuferatu <kuznetsovpf@gmail.com>
2023-04-23 23:51:59 +0300

    add task2 solution

--- Program.cs
+++ Program.cs
@@ -17,3 +17,26 @@ for (int i = 1; i < numberB; i++)
 step = step * numberA;
 }
 Console.WriteLine($"A в степени B = {step}");
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// Например:
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+int count = number % 10;
+number = number / 10;
+sum = sum + count;
+}
+
+Console.WriteLine($"Сумма цифр в числе = {sum}");
