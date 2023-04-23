f60e5c908429039eb224af64434959e118e1aa39
nosuferatu <kuznetsovpf@gmail.com>
2023-04-24 00:00:19 +0300

    add task3 solution

--- Program.cs
+++ Program.cs
@@ -40,3 +40,27 @@ sum = sum + count;
 }
 
 Console.WriteLine($"Сумма цифр в числе = {sum}");
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// Например:
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+Console.Clear();
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + extract (i) + " ");
+ }
+Console.Write("]");
+
+int extract (int t)
+{
+    return numbers[t];
+}
